12b4360abe5d3b6fa361a2bd2afee64167aa8e4a
NurannNalci <nuran.nalann2411@gmail.com>
2022-06-12 14:15:58 +0200

    HW008

---
+++ src/Dortgen.java
@@ -0,0 +1,8 @@
+public interface Dortgen {
+    void setGenislik(double genislik);
+    double getGenislik();
+    void setDerinlik();
+    double getDerinlik();
+    double alanHesapla();
+    double hacimHesapla();
+}
